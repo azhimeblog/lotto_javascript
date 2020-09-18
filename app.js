e70dd9eab58ee81bb44229898abc0e0b28e1b839
azhimeblog <azhime.blog@gmail.com>
2020-09-18 17:54:43 +0700

    updatejavascript ep12

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-let name = prompt("ใส่ชื่อของคุณ:")
-document.getElementById("name").innerHTML = name
+//let name = prompt("ใส่ชื่อของคุณ:")
+//document.getElementById("name").innerHTML = name
 
 let lotto_number = prompt("กรอกเลขที่ต้องการซื้อ:")
 document.getElementById("lotternumber").innerHTML = lotto_number
-document.getElementById("result").innerHTML = Math.floor(Math.random() * 100)
+    //document.getElementById("result").innerHTML = Math.floor(Math.random() * 100)
+let final_number = document.getElementById("result").innerHTML = Math.floor(Math.random() * 100)
+
+
+
+//console.log(32 == "32")
+
+//console.log(lotto_number == final_number)
+if (lotto_number == final_number) {
+    greeting = "คุณถูกรางวัล";
+} else {
+    greeting = "คุณไม่ถูกรางวัล";
+}
