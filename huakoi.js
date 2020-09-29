fec124056d5b7d87c5dedf1c1a180f12e800c05f
azhimeblog <azhime.blog@gmail.com>
2020-09-29 17:59:09 +0700

    huakoi

--- huakoi.js
+++ huakoi.js
@@ -3,29 +3,20 @@ let score = 0;
 for (i = 1; i <= round; i++) {
     var ans = prompt("คุณคิดว่าตานี้จะออก หัว  หรือ  ก้อย  ");
     var random_ans = ""
-    if (Math.floor(Math.random() * 10) <= 4)
-
-    {
+    if (Math.floor(Math.random() * 10) <= 4) {
         random_ans = "หัว";
-
     } else {
-
         random_ans = "ก้อย";
-
     }
-
     if (ans == random_ans) {
         score = score + 1;
-
-
+        alert("คุณชนะได้คะแนนไป 1 คะแนน")
     } else {
         score = score;
-
+        alert("คุณแพ้ลองอีกทีนะ")
     }
-
     document.getElementById("round").innerHTML = "คุณเล่นทั้งหมด  " + round + "   เกม"
     document.getElementById("ans").innerHTML += " <b>เกมที่ " + i + " </b>เกมนี้ออก  " + random_ans + " <br>"
-
     document.getElementById("result").innerHTML = "คะแนนที่คุณได้คือ   " + score + "   คะแนน"
 
 }
